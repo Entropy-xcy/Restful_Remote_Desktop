02e01df0ddc1c62aa6f802fcc8316e43b67cf441
Entropy Xu <entropy.xcy@protonmail.com>
2018-11-10 13:47:31 -0800

    Finish basic functions

--- client.py
+++ client.py
@@ -1,15 +1,21 @@
 import base64
 from PIL import Image
 import numpy as np
-import cv2
+import time
 import requests
 import io
 
 
 class Server:
-    def __init__(self, host, port):
+    def __init__(self, host, port=80, display=True):
         self.host = host
         self.port = port
+        if display:
+            print(str(host) + ":" + str(port) + " Initializing...")
+        self.width, self.height = self.get_screen_size()
+        if display:
+            print(str(host) + ":" + str(port) + " Initialized")
+            print("Width:" + str(self.width) + "  Height:" + str(self.height))
 
     # Return the screen shot's base64 string
     def get_screen_base64(self):
@@ -27,8 +33,77 @@ class Server:
         img = np.array(img)
         return img
 
+    # Return the screen size of the server
+    def get_screen_size(self):
+        url = "http://" + str(self.host) + ':' + str(self.port) + '/get_screen_size'
+
+        r = requests.get(url)
+        assert r.status_code == 200
+        size = r.text.split("*")
+        width = int(size[0])
+        height = int(size[1])
+        return width, height
+
+    # Move the cursor relative to current position on the server
+    def move_cursor(self, del_x, del_y, duration=0):
+        url = "http://" + str(self.host) + ':' + str(self.port) + '/move_cursor'
+        payload = {'del_x': str(del_x), 'del_y': str(del_y), "duration": str(duration)}
+        r = requests.get(url, params=payload)
+
+        assert r.status_code == 200
+        assert r.text == "0"
+
+    # Move the cursor to position (x,y)
+    def move_cursor_to(self, x, y, duration=0):
+        url = "http://" + str(self.host) + ':' + str(self.port) + '/move_cursor_to'
+        payload = {'x': str(x), 'y': str(y), "duration": str(duration)}
+        r = requests.get(url, params=payload)
+
+        assert r.status_code == 200
+        assert r.text == "0"
+
+    # Handle Mouse Key Event
+    def mouse_key(self, key='left', event='click'):
+        url = "http://" + str(self.host) + ':' + str(self.port) + '/mouse_key'
+        payload = {'key': str(key), 'event': str(event)}
+        r = requests.get(url, params=payload)
+
+        assert r.status_code == 200
+        assert r.text == "0"
+
+    # Keyboard down
+    def key_down(self, key):
+        url = "http://" + str(self.host) + ':' + str(self.port) + '/key_down'
+        payload = {'key': str(key)}
+        r = requests.get(url, params=payload)
+
+        assert r.status_code == 200
+        assert r.text == "0"
+
+    # Keyboard up
+    def key_up(self, key):
+        url = "http://" + str(self.host) + ':' + str(self.port) + '/key_up'
+        payload = {'key': str(key)}
+        r = requests.get(url, params=payload)
+
+        assert r.status_code == 200
+        assert r.text == "0"
+
+    # Type message
+    def type_write(self, message, delay=0):
+        url = "http://" + str(self.host) + ':' + str(self.port) + '/type_write'
+        payload = {'message': str(message), 'delay': str(delay)}
+        r = requests.get(url, params=payload)
+
+        assert r.status_code == 200
+        assert r.text == "0"
+
 
 if __name__ == "__main__":
     s0 = Server('localhost', 80)
-    cv2.imshow("a", s0.get_screen())
-    cv2.waitKey()
+    time.sleep(2)
+    s0.key_down('win')
+    s0.key_down('r')
+    s0.key_up('r')
+    time.sleep(1)
+    s0.key_up('win')
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask import request
 import pyautogui
 import numpy as np
 import cv2
@@ -7,9 +8,12 @@ import base64
 app = Flask(__name__)
 
 
-@app.route('/get_info')
-def get_info():
-    return "info"
+# Return the size of the screen
+@app.route('/get_screen_size')
+def get_screen_size():
+    image = pyautogui.screenshot()
+    image = np.array(image)
+    return str(len(image[0]))+'*'+str(len(image))
 
 
 # Return the string of base64 containing the image
@@ -27,34 +31,66 @@ def get_screen():
     return b64_str
 
 
+# Move the cursor relative to the current position
 @app.route('/move_cursor')
 def move_cursor():
-    return "move cursor"
-
-
-@app.route('/left_down')
-def left_down():
-    return "cursor down"
-
-
-@app.route('/left_up')
-def left_up():
-    return "cursor up"
+    del_x = request.args.get('del_x', default=0, type=int)
+    del_y = request.args.get('del_y', default=0, type=int)
+    duration = request.args.get('duration', default=0, type=float)
+    pyautogui.moveRel(del_x, del_y, duration)
+    return "0"
 
 
 @app.route('/move_cursor_to')
 def move_cursor_to():
-    return "move cursor to"
+    x = request.args.get('x', default=0, type=int)
+    y = request.args.get('y', default=0, type=int)
+    duration = request.args.get('duration', default=0, type=float)
+    pyautogui.moveTo(x, y, duration)
+    return "0"
+
+
+@app.route('/mouse_key')
+def mouse_key():
+    # key could be 'left' 'middle' 'right'
+    key = request.args.get('key', default='left', type=str)
+    # event could be 'down' 'up' 'click'
+    event = request.args.get('event', default='click', type=str)
+    if event == 'click':
+        if key == 'left':
+            pyautogui.click()
+        elif key == 'middle':
+            pyautogui.middleClick()
+        elif key == 'right':
+            pyautogui.rightClick()
+    elif event == 'down':
+        pyautogui.mouseDown(button=key)
+    elif event == 'up':
+        pyautogui.mouseUp(button=key)
+
+    return "0"
 
 
 @app.route('/key_down')
 def key_down():
-    return "Key down"
+    key = request.args.get('key', default='', type=str)
+    pyautogui.keyDown(key)
+    return "0"
 
 
 @app.route('/key_up')
 def key_up():
-    return "keyup"
+    key = request.args.get('key', default='', type=str)
+    pyautogui.keyUp(key)
+    return "0"
+
+
+@app.route('/type_write')
+def type_write():
+    message = request.args.get('message', default='', type=str)
+    delay = request.args.get('delay', default=0, type=float)
+    pyautogui.typewrite(message, delay)
+    return "0"
 
 
 if __name__ == "__main__":
